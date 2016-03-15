42e7dbde06e8ff8758e5168b03a3ef826f665f2a
Luis Fernando Gomes <dev@luiscoms.com.br>
2016-03-14 23:18:33 -0300

    Added scritp to test

---
+++ docker/docker-test.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+CUR_DIR=$(dirname "$(readlink -f "$0")")
+source ${CUR_DIR}/docker-config.sh
+
+PJ_ROOT=$(dirname ${CUR_DIR})
+
+echo ${DOCKER_FULLTAG}
+docker run --rm -it -v ${PJ_ROOT}:/app ${DOCKER_FULLTAG} py.test
