fddb7a133c69128694f1582c42962bce13647adb
Luis Fernando Gomes <dev@luiscoms.com.br>
2016-07-08 10:39:07 -0300

    Change defautl port

--- docker/docker-run.sh
+++ docker/docker-run.sh
@@ -5,4 +5,4 @@ source ${CUR_DIR}/docker-config.sh
 
 PJ_ROOT=$(dirname ${CUR_DIR})
 
-docker run -d --name ${DOCKER_NAME} -p 80:80 -v ${PJ_ROOT}:/app ${DOCKER_FULLTAG}
+docker run -d --name ${DOCKER_NAME} -p 80:8080 -v ${PJ_ROOT}:/app ${DOCKER_FULLTAG}
